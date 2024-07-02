d470e454b661d4ac8549c0eb6c79c871d3477d1e
Nike299792458 <Nikoletognetti@gmail.com>
2024-07-02 18:52:41 +0200

    doublers ma non ci pensano neanche a funzionare

--- doublers.jl
+++ doublers.jl
@@ -7,25 +7,25 @@ notazione= stvol= spacetime volume
 STDIM = 2 #spacetime dimensionality
 
 #O1 = 1/(Nt*Ns^{STDIM-1}) Σ_n (mhat^2 ϕ_n^2)
-function O1(stvol::Int, mhat::Float64, lattice::Array{Float64})
+function O1d(stvol::Int, mhat::Float64, lattice::Array{Float64})
    ris= mhat*mhat*dot(lattice,lattice)/stvol
     return ris
 end
 
-#O2 = 1/(Nt*Ns^{STDIM-1}) Σ_n Σ_{mu>0} (ϕ(n+μ)-ϕ_n)^2
-function O2(stvol::Int, Nt::Int, lattice::Array{Float64})
+#O2 = 1/(4*Nt*Ns^{STDIM-1}) Σ_n Σ_{mu>0} (ϕ(n+μ)-ϕ_(n-μ))^2
+function O2d(stvol::Int, Nt::Int, lattice::Array{Float64})
     lattice=reshape(lattice, :)
-    ris= dot(circshift(lattice,-Nt)-lattice,circshift(lattice,-Nt)-lattice)/stvol
+    ris= dot(circshift(lattice,-Nt)-circshift(lattice,+Nt),circshift(lattice,-Nt)-circshift(lattice,+Nt))/(stvol*4)
     return ris
 end
-#// O3 = 1/(Nt*Ns^{STDIM-1}) Σ_n (ϕ_(n+0)-ϕ_n)^2
-function O3(stvol::Int, lattice::Array{Float64})
+#// O3 = 1/(4*Nt*Ns^{STDIM-1}) Σ_n (ϕ_(n+0)-ϕ_(n-0))^2
+function O3d(stvol::Int, lattice::Array{Float64})
     rows = [collect(row) for row in eachcol(lattice)]
     a=0
     for i in 1:size(lattice,1)
-    a+=dot(circshift(rows[i],-1)-rows[i],circshift(rows[i],-1)-rows[i])
+    a+=dot(circshift(rows[i],-1)-circshift(rows[i],+1),circshift(rows[i],-1)-circshift(rows[i],+1))
     end
-    ris= a/stvol
+    ris= a/(stvol*4)
     return ris
 end
 #=
@@ -34,24 +34,24 @@ per passare dal singolo indice r ai due indici (i,j):
 j= celing-division tra 1 e Nt e i=modulo in base Nt di r
 =#
 
-function Σ_n(lattice::Array{Float64}, r::Int, Nt::Int, Ns::Int)
+function Σ_nd(lattice::Array{Float64}, r::Int, Nt::Int, Ns::Int)
     i=mod1(r,Nt)
     j=cld(r,Nt)
-    lattice[mod1(i+1, Nt), j] + lattice[mod1(i-1, Nt), j] +
-                   lattice[i, mod1(j+1, Ns)] + lattice[i, mod1(j-1, Ns)]
+    lattice[mod1(i+2, Nt), j] + lattice[mod1(i-2, Nt), j] +
+                   lattice[i, mod1(j+2, Ns)] + lattice[i, mod1(j-2, Ns)]
 end
     
 
-function heathbath!(lattice::Array{Float64}, r::Int, mhat::Float64, Nt::Int, Ns::Int)
-    std = 1.0/sqrt(mhat*mhat+2.0*STDIM)
-    avg = Σ_n(lattice, r, Nt, Ns)/(mhat*mhat+2.0*STDIM)
+function heathbathd!(lattice::Array{Float64}, r::Int, mhat::Float64, Nt::Int, Ns::Int)
+    std = 1.0/sqrt(mhat*mhat+0.5*STDIM)
+    avg = Σ_nd(lattice, r, Nt, Ns)/(mhat*mhat+0.5*STDIM)/4
     lattice[r] = avg+std*randn()
 
     return 1
 end
 
-function overrelax!(lattice::Array{Float64}, r::Int, mhat::Float64, Nt::Int, Ns::Int)
-    avg =  Σ_n(lattice, r, Nt, Ns)/(mhat*mhat+2.0*STDIM)
+function overrelaxd!(lattice::Array{Float64}, r::Int, mhat::Float64, Nt::Int, Ns::Int)
+    avg =  Σ_nd(lattice, r, Nt, Ns)/(mhat*mhat+0.5*STDIM)/4
     new = 2.0*avg-lattice[r]
     lattice[r] = new
 
---
+++ doublers_main.jl
@@ -0,0 +1,91 @@
+using ArgParse, Dates, DelimitedFiles, Random, Printf, LinearAlgebra
+include("doublers.jl")
+
+function parse_cmd()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "ratio"
+            help = "Ratio between spatial and temporal size of the lattice"
+            required = true
+            arg_type = Int
+        "sample"
+            help = "Number of steps in the simulation"
+            required = true
+            arg_type = Int
+        "--path", "-p"
+            help = "The path where files are stored"
+            default = joinpath(["..", "simulations_c"])
+            required = false
+            arg_type = String
+        
+    end
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_cmd()
+    ratio = parsed_args["ratio"]
+    sample = parsed_args["sample"]
+    path = parsed_args["path"]
+   
+
+    
+    println(@sprintf "Starting simulation: sample=%.1e ratio=%.i " sample ratio )
+
+    timestamps=[4,5,6,7,8,10]
+    for Nt in timestamps
+        Ns=ratio*Nt
+        # initializing...
+        lattice = zeros(Float64, Nt,Ns)
+        acc = 0
+        
+        
+        # simulation parameters
+        orsteps = 5
+        measevery = 10
+        Nt_bar= 10000 
+        mhat=1/Nt
+        stvol=Nt #stvol=Nt*Ns^{STDIM-1}
+        stvol_bar= Ns*Nt_bar
+        for i in 1:(STDIM-1)
+            stvol=stvol*Ns
+        end
+        # files management
+        if !isdir(path)
+            mkpath(path)
+        end
+        fname = @sprintf "free_scalar_th_sample=%.1eratio=%.iNt=%2.2iNs=%2.2i.txt" sample ratio Nt Ns
+        fr = joinpath([path, fname])
+        if !isfile(fr)
+            touch(fr)
+        end
+        # writing header
+        open(fr, "w") do infile
+            writedlm(infile, ["obs1" "obs2" "obs3"], " ")
+        end
+        
+        start = now()
+        datafile = open(fr, "a")
+        for iter in 1:sample
+            for r in LinearIndices(lattice)
+                acc+=heathbathd!(lattice, r, mhat, Nt, Ns)
+                for _ in 1:orsteps
+                    acc+=overrelaxd!(lattice, r, mhat, Nt, Ns)
+                end
+            end
+
+            if iter%measevery == 0
+                obs1 = O1d(stvol, mhat,lattice)-O1d(stvol_bar, mhat,lattice)
+                obs2 = O2d(stvol, Nt,lattice)-O2d(stvol_bar, Nt_bar,lattice)
+                obs3 = O3d(stvol,lattice)-O3d(stvol_bar,lattice)
+                writedlm(datafile, [obs1 obs2 obs3], " ")
+            end
+            
+        end
+        close(datafile)
+        elapsed = Dates.canonicalize(Dates.round((now()-start), Dates.Second))
+        println("\n$(round(now(), Dates.Second));\nNₜ = $Nt,Ns = $Ns, elapsed time $(elapsed)\n")
+    end
+end
+main()
