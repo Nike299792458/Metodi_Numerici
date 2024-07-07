8de385d52cb531d8ee4b693aa1f73846912a930b
Nike299792458 <Nikoletognetti@gmail.com>
2024-07-07 23:43:02 +0200

    ordine input da tastiera

--- sim_exec_c/sim_T=m.sh
+++ sim_exec_c/sim_T=m.sh
@@ -3,13 +3,12 @@
 path="../simulations_c/"
 sample=5000000
 # Array to loop over
-Nt=(4 6 8 10)
-ratio=5
+Nt=(4 6 8 7 10)
+ratio=4
 
 
 for Nti in "${Nt[@]}"
 do 
 	julia th_free_scalar.jl  "$ratio" "$Nti" "$sample"
 done
-
 
