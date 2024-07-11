9637aa8d8ce5d23b3bcea8bf65bb733a284fce2f
Nike299792458 <Nikoletognetti@gmail.com>
2024-07-11 12:00:04 +0200

    modifice .sh

--- sim_exec_c/spectrum.sh
+++ sim_exec_c/spectrum.sh
@@ -4,8 +4,8 @@
 path="../simulations_c/spectrum_c"
 sample=100000000
 
-Nt=60
-
+Nt=(60)
+for Nti in "${Nt[@]}"
 do 
-	julia spectrum_analysis.jl   "$Nt" "$sample"
+	julia spectrum_analysis.jl   "$Nti" "$sample"
 done
